8b58adaa79933da8f3686f6a1bd75fcfa277c7e5
Quoc Cuong <89184885+Quoccuong1004@users.noreply.github.com>
2022-06-29 14:47:12 +0700

    Update class BubbleSort

--- Javafx/src/SortingAlgorithm/BubbleSort.java
+++ Javafx/src/SortingAlgorithm/BubbleSort.java
@@ -1,17 +1,85 @@
 package SortingAlgorithm;
 
-public class BubbleSort extends Sort implements Display{
-    void bubbleSort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-    }
+import java.util.ArrayList;
+
+public class BubbleSort extends Sort implements Display {
+	// Attribute storing indices of two switching elements at the i step as an array
+	// (= currentSwitchIndex.get(i))
+	private ArrayList<int[]> currentSwitchIndex;
+
+	// Constructors
+	public BubbleSort(int arrayLength) {
+		super(arrayLength);
+		currentSwitchIndex = new ArrayList<int[]>();
+	}
+
+	public BubbleSort(int[] inputArray) {
+		super(inputArray);
+		currentSwitchIndex = new ArrayList<int[]>();
+	}
+
+	// Methods
+	public int[] getCurrentSwitchIndex(int stepNumber) {
+		return currentSwitchIndex.get(stepNumber);
+	}
+
+	public int[] displayStart() {
+		currentSteps = 0;
+		return arrayOfSteps.get(0);
+	}
+
+	public int[] displayFinish() {
+		currentSteps = this.getNumberSteps();
+		return arrayOfSteps.get(this.getNumberSteps() - 1);
+	}
+
+	public int[] nextStep() {
+		currentSteps += 1;
+		return arrayOfSteps.get(this.currentSteps + 1);
+	}
+
+	public int[] preStep() {
+		currentSteps -= 1;
+		return arrayOfSteps.get(this.currentSteps - 1);
+	}
+
+	public void sort() {
+		numberSteps = 0;
+		arrayOfSteps.add(arr.clone());
+		// Bubble Sort
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int[] tmp = new int[2];
+					tmp[0] = j;
+					tmp[1] = j + 1;
+					currentSwitchIndex.add(tmp);
+					int tmp2 = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp2;
+					arrayOfSteps.add(arr.clone());
+					numberSteps++;
+
+				}
+
+			}
+		}
+	}
+
+	// test
+	public static void main(String[] args) {
+		BubbleSort s = new BubbleSort(10);
+		s.sort();
+		s.displayStart();
+		System.out.print(Sort.displayStep(s.arrayOfSteps, s.currentSteps) + " ");
+		System.out.print(s.getCurrentSwitchIndex(s.currentSteps)[0] + " ");
+		System.out.println(s.getCurrentSwitchIndex(s.currentSteps)[1]);
+		for (int i = 0; i < 51; i++) {
+			s.nextStep();
+			System.out.print(Sort.displayStep(s.arrayOfSteps, s.currentSteps) + " ");
+			System.out.print(s.getCurrentSwitchIndex(s.currentSteps)[0] + " ");
+			System.out.println(s.getCurrentSwitchIndex(s.currentSteps)[1]);
+		}
+	}
 
 }
