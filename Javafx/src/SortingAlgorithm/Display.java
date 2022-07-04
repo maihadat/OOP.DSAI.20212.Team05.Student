053e2f70dc0f77f8bf6a65d7003534a44f88f230
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2022-07-04 13:13:49 +0700

    Delete Javafx directory

--- Javafx/src/SortingAlgorithm/Display.java
+++
@@ -1,8 +0,0 @@
-package SortingAlgorithm;
-
-public interface Display {
-	public int[] displayStart();
-	public int[] displayFinish();
-	public int[] nextStep();
-	public int[] preStep();
-}
--- Javafx/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Javafx {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
