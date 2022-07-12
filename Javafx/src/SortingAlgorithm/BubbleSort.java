e158154181d458582cc40db01c761397cac486a7
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2022-07-12 10:33:47 +0700

    Add Back button to sort and refactor

---
+++ Javafx/src/SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,36 @@
+package SortingAlgorithm;
+
+public class BubbleSort extends Sort implements Display {
+	// Constructors
+	public BubbleSort(int arrayLength) {
+		super(arrayLength);
+	}
+
+	public BubbleSort(int[] inputArray) {
+		super(inputArray);
+	}
+
+	// Methods
+	public void sort() {
+		numberSteps = 0;
+		arrayOfSteps.add(arr.clone());
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int[] tmp = new int[2];
+					tmp[0] = j;
+					tmp[1] = j + 1;
+					currentSwitchIndex.add(tmp);
+					int tmp2 = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp2;
+					arrayOfSteps.add(arr.clone());
+					numberSteps++;
+
+				}
+
+			}
+		}
+	}
+
+}
